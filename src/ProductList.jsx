3d4437e63af326deb863695d34e7e8afc7a86a8f
sindhu <sindhu.amma@gmail.com>
2025-10-27 06:20:47 -0400

    final commit

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -7,6 +7,7 @@ function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
+    
     const plantsArray = [
         {
             category: "Air Purifying Plants",
